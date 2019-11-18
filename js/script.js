f3a3a2655cadb7076478f17abfa96446e22e5c4c
Sven Knockaert <sven.knockaert@hotmail.com>
2019-11-18 22:57:41 +0100

    Refactor some things

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 (() => {
 
-  let OFFSET_RACKET_CENTER = {x: 65, y: 40};
+  let OFFSET_RACKET_CENTER = { x: 65, y: 40 };
+  let RACKET_SIZE = 90; // diameter
+  let BALL_SIZE = 40; // diameter
+  let debugEnabled = false;
 
   function init() {
     document.addEventListener("DOMContentLoaded", () => {
@@ -13,7 +16,7 @@
     let tennisball = document.querySelector(".ball");
     let debugOverlay = document.querySelector(".debug");
     let followMouse = false;
-    let dist = { x: 0, y: 0 };
+    let dist = { x: 0, y: 0 }; // position mouse clicked on racket, this is pure for visuals
 
     racket.addEventListener("click", (event) => {
       followMouse = !followMouse;
@@ -24,18 +27,36 @@
 
     document.addEventListener("mousemove", (event) => {
       if (!followMouse) return;
+
       racket.style.left = (event.clientX + dist.x).toString() + "px";
       racket.style.top = (event.clientY + dist.y).toString() + "px";
-      debugOverlay.style.left = (racket.offsetLeft + OFFSET_RACKET_CENTER.x).toString() + "px";
-      debugOverlay.style.top = (racket.offsetTop + OFFSET_RACKET_CENTER.y).toString() + "px";
+      if (debugEnabled) updateDebugPosition(racket, debugOverlay);
+
       let ballLaunched = tennisballRacketColliding(tennisball, racket);
-      console.log(ballLaunched);
+      if (ballLaunched) {
+
+      }
+      // TODO:
+      // 1. if ball launched, set a variable to false so it cant be hit again
+      // 2. launch it effectively, by calculating angle of hit
+      // 3. check for collisions with words
+      // 4. just play the animation on those words
+      // 5. ??
+      // 6. profit
     });
   }
 
+  function angleOf(p1, p2) {
+    // NOTE: Remember that most math has the Y axis as positive above the X.
+    // However, for screens we have Y as positive below. For this reason, 
+    // the Y values are inverted to get the expected results.
+    let deltaY = (p1.y - p2.y);
+    let deltaX = (p2.x - p1.x);
+    let result = Math.toDegrees(Math.atan2(deltaY, deltaX));
+    // return (result < 0) ? (360d + result) : result;
+  }
+
   function tennisballRacketColliding(ball, racket) {
-    let BALL_SIZE = 40;
-    let RACKET_SIZE = 90;
     let ballPos = {
       x: ball.offsetLeft + BALL_SIZE / 2,
       y: ball.offsetTop + BALL_SIZE / 2,
@@ -48,7 +69,7 @@
     let dx = ballPos.x - racketPos.x;
     let dy = ballPos.y - racketPos.y;
     let distance = Math.sqrt(dx * dx + dy * dy);
-    return (distance < (BALL_SIZE / 2 + RACKET_SIZE/2));
+    return (distance < (BALL_SIZE / 2 + RACKET_SIZE / 2));
   }
 
   function tennisballElementColliding(tennisball, element) {
@@ -80,12 +101,11 @@
     return (dx * dx + dy * dy <= (circle.r * circle.r));
   }
 
-  function getClientSizes() {
-    return {
-      width: Math.max(document.body.clientWidth),
-      height: Math.max(document.body.clientHeight),
-    };
+  function updateDebugPosition(racket, debugOverlay) {
+    debugOverlay.style.left = (racket.offsetLeft + OFFSET_RACKET_CENTER.x).toString() + "px";
+    debugOverlay.style.top = (racket.offsetTop + OFFSET_RACKET_CENTER.y).toString() + "px";
   }
+
   init();
 
 })();
